6e033e156ebf84ae269371e0bfdf3240060cac9e
Kevin Pulo <kevin.pulo@mongodb.com>
2019-08-07 11:13:27 +1000

    extra y key in middle of top alpha row

--- keyboards/xd75/keymaps/devkev/keymap.c
+++ keyboards/xd75/keymaps/devkev/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | `      | 1      | 2      | 3      | 4      | 5      | 6      | 6      | 7      | 8      | 9      | 0      | -      | =      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * |        | TAB    | Q      | W      | E      | R      | T      |        | Y      | U      | I      | O      | P      | [      | ]      |
+ * |        | TAB    | Q      | W      | E      | R      | T      | Y      | Y      | U      | I      | O      | P      | [      | ]      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * |        | CAP LK | A      | S      | D      | F      | G      |        | H      | J      | K      | L      | ;      | '      | ENTER  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-    _______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+    _______, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
     _______, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
     _______, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT,
     _______, _______, _______, KC_LGUI, KC_LALT, MO(_FN), _______, _______, KC_SPC,  KC_SPC,  MO(_FN), _______, KC_LEFT, KC_DOWN, KC_RGHT
